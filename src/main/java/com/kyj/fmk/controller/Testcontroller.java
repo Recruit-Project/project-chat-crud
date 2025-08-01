972a44b428f63339d051926e3737a4af3e8106ea
kimyj <yongjjun8718@gmail.com>
2025-08-02 01:29:44 +0900

    init

---
+++ src/main/java/com/kyj/fmk/controller/Testcontroller.java
@@ -0,0 +1,18 @@
+package com.kyj.fmk.controller;
+
+import com.kyj.fmk.sec.annotation.PublicEndpoint;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/chat")
+public class Testcontroller {
+
+    @PublicEndpoint
+    @GetMapping("/")
+    public String test(){
+        return "chat";
+    }
+
+}
